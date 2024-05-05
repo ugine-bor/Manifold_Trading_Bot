37f3c74bc9276c9acfbf1635e9d3e5c54f75e173
ugine <uginebor69@gmail.com>
2024-05-05 12:48:47 +0200

    Better print

--- app.py
+++ app.py
@@ -27,25 +27,47 @@ class Bot:
         else:
             return f"Unknown Error - {res}"
 
-    def ping(self):
+    def ping(self, **kwargs):
         url = f"{self.root_url}/v0/markets?limit=1"
         response = requests.get(url)
         return self.__response_code(response)
 
-    def get_user_info(self, name):
+    def get_user_info(self, name, **kwargs):
         url = f"{self.root_url}/v0/user/{name}"
         response = requests.get(url, headers={"Authorization": f"Bearer {self.api_key}"})
         return self.__response_code(response)
 
-    def get_user_info_lite(self, name):
+    def get_user_info_lite(self, name, **kwargs):
         url = f"{self.root_url}/v0/user/{name}/lite"
         response = requests.get(url, headers={"Authorization": f"Bearer {self.api_key}"})
         return self.__response_code(response)
+
+    def get_user_by_id(self, id, **kwargs):
+        url = f"{self.root_url}/v0/user/by-id/{id}"
+        response = requests.get(url, headers={"Authorization": f"Bearer {self.api_key}"})
+        return self.__response_code(response)
+
+    @staticmethod
+    def __print(method):
+        def wrapper(*args, **kwargs):
+            result = method(*args, **kwargs)
+
+            to_print = kwargs.pop("pt", False)
+            if to_print:
+                print(f"\nMethod: {method.__name__}\nResult: {result}\n")
+            return result
+
+        return wrapper
+
+    ping = __print(ping)
+    get_user_info = __print(get_user_info)
+    get_user_info_lite = __print(get_user_info_lite)
+    get_user_by_id = __print(get_user_by_id)
 
 
 load_dotenv()
 
 bot = Bot(getenv("API_KEY"))
-print(bot.ping())
-print(bot.get_user_info("And"))
-print(bot.get_user_info_lite("And"))
+bot.ping(pt=True)
+bot.get_user_info("And")
+bot.get_user_info_lite("And", pt=True)
