99ad84a27b546aaaa77af5fc9ea92ca5d3b209e7
ugine <uginebor69@gmail.com>
2024-05-05 14:43:33 +0200

    get groups before time

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import requests
 from dotenv import load_dotenv
 from os import getenv
 
+from datetime import datetime
+
 
 class Bot:
     def __init__(self, api_key, root_url="https://api.manifold.markets"):
@@ -55,6 +57,29 @@ class Bot:
         response = requests.get(url, headers={"Authorization": f"Key {self.api_key}"})
         return self.__response_code(response)
 
+    def get_groups(self, **kwargs):
+        url = f"{self.root_url}/v0/groups"
+
+        before_time = kwargs.pop("before", None)
+        if before_time:
+            before_time = before_time.ljust(23, '0') if ' ' in before_time else before_time + '.000'
+            before_time = str(datetime.strptime(before_time, "%Y.%m.%d %H:%M:%S.%f").timestamp()).replace('.', '')
+
+        available_to = kwargs.pop("available_to_id", None)
+        response = requests.get(url, params={"beforeTime": before_time, "availableToUserId": available_to})
+
+        return self.__response_code(response)
+
+    def get_group_by_slug(self, slug, **kwargs):
+        url = f"{self.root_url}/v0/group/{slug}"
+        response = requests.get(url)
+        return self.__response_code(response)
+
+    def get_group_by_id(self, id, **kwargs):
+        url = f"{self.root_url}/v0/group/by-id/{id}"
+        response = requests.get(url)
+        return self.__response_code(response)
+
     @staticmethod
     def __print(method):
         def wrapper(*args, **kwargs):
@@ -62,7 +87,8 @@ class Bot:
 
             to_print = kwargs.pop("pt", False)
             if to_print:
-                print(f"\nMethod: {method.__name__} {'(lite)' if kwargs.pop('lite', False) else ''}\nResult: {result}\n")
+                print(
+                    f"\nMethod: {method.__name__} {'(lite)' if kwargs.pop('lite', False) else ''}\nResult: {result}\n")
             return result
 
         return wrapper
@@ -71,9 +97,21 @@ class Bot:
     get_user_info = __print(get_user_info)
     get_user_by_id = __print(get_user_by_id)
     get_me = __print(get_me)
+    get_groups = __print(get_groups)
+    get_grpoup_by_slug = __print(get_group_by_slug)
 
 
 load_dotenv()
+
+
+def print_all(lst):
+    if isinstance(lst, list):
+        for item in lst:
+            print(item)
+            pass
+    else:
+        print(lst)
+
 
 # pt = print to console
 
@@ -87,3 +125,8 @@ bot.get_user_by_id("hKURbhPsW2VpezOdAAisTTCIBLn2", pt=True)
 bot.get_user_by_id("hKURbhPsW2VpezOdAAisTTCIBLn2", lite=True, pt=True)
 
 bot.get_me(pt=True)
+
+groups = bot.get_groups(before='2023.01.04 22:11:05.123000', available_to_id="hKURbhPsW2VpezOdAAisTTCIBLn2")
+print_all(groups)
+
+bot.get_grpoup_by_slug("astroworld", pt=True)
